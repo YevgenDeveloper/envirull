87cafcbd209b888aafffa9f95d7c4e56a2408098
Yevgen <eugen.developer.it@gmail.com>
2021-10-18 23:26:27 +0000

    Argv update

--- src/argv.ts
+++ src/argv.ts
@@ -1,6 +1,9 @@
-const noParseSplitter = "--";
+const NO_PARSE_AFTER = "--";
+const OPT_START_WITH_EQUAL = /^--.+=/;
+const OPT_WIDTH_EQUAL = /^--([^=]+)=([\s\S]*)$/;
+export type ParsedArg = string | boolean | number;
 export type ParsedArgs = {
-	[key: string]: string | boolean | Array<string>;
+	[key: string]: ParsedArg | Array<ParsedArg>;
 	_: Array<string>;
 }
 export default function parse(args: Array<string>): ParsedArgs {
@@ -9,9 +12,9 @@ export default function parse(args: Array<string>): ParsedArgs {
 	return argv;
 }
 function noParseSplit(args: Array<string>): [Array<string>, Array<string>] {
-	if (args.indexOf(noParseSplitter) !== -1) {
+	if (args.indexOf(NO_PARSE_AFTER) !== -1) {
 		return [
-			args.slice(args.indexOf(noParseSplitter) + 1),
+			args.slice(args.indexOf(NO_PARSE_AFTER) + 1),
 			args.slice(0, args.indexOf('--'))
 		];
 	}
@@ -20,4 +23,28 @@ function noParseSplit(args: Array<string>): [Array<string>, Array<string>] {
 function parseArrayOfArgv(args: Array<string>) {
 	args.forEach((arg) => {
 	});
+}
+function isOptionWithEqualValue(arg: string) {
+	if (!OPT_START_WITH_EQUAL.test(arg)) {
+	}
+	const matched = arg.match(OPT_WIDTH_EQUAL)!;
+	const [key, value] = [matched[1], matched[2]];
+	if (flags.bools[key]) {
+		value = value !== 'false';
+	}
+	setArg(key, value, arg);
+}
+function asTyped(value: string): ParsedArg {
+	if (isNumber(value)) {
+		return Number(value);
+	}
+}
+function isNumber(value: any): boolean {
+	if (typeof value === 'number') {
+		return true;
+	}
+	if (/^0x[0-9a-f]+$/i.test(value)) {
+		return true;
+	}
+	return /^[-+]?(?:\d+(?:\.\d*)?|\.\d+)(e[-+]?\d+)?$/.test(value);
 }
