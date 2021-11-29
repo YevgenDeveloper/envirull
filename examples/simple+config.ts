4bf86691062a51e7033c1d05e3b57e3ea09c1fb3
Yevgen <eugen.developer.it@gmail.com>
2021-11-29 19:52:12 +0000

    REmove ts examples

--- examples/simple+config.ts
+++
@@ -1,5 +0,0 @@
-import envfull from "../src/index";
-const parsed = envfull(process)("/path/to/config.json");
-console.log(parsed.$); 
-console.log(parsed._); 
-console.log(parsed["--"]); 
--- examples/simple.ts
+++
@@ -1,5 +0,0 @@
-import envfull from "../src/index";
-const parsed = envfull(process)();
-console.log(parsed.$); 
-console.log(parsed._); 
-console.log(parsed["--"]); 
